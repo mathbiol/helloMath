014eecf5e14be810601de609d0509396dfd73a6c
Jonas Almeida <jalmeida@mathbiol.org>
2017-02-16 10:56:01 -0500

    port from env

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello Math at '+Date()+' !')
 })
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var port = process.env.PORT || 3000
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!')
 })
