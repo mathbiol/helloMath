93a8c025bc4edd859400ed12e82deb3aa8efe442
Jonas Almeida <jalmeida@mathbiol.org>
2017-02-16 12:39:03 -0500

    back to hello math

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Wade at '+Date()+' !')
+  res.send('Hello Math at '+Date()+' !')
 })
 var port = process.env.PORT || 3000
 app.listen(port, function () {
